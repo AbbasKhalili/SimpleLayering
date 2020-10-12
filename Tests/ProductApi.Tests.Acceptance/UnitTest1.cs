20b475f6e0ee0c8e6ce17e6b0f75e6e4fdf82a9f
AbbasKhalili <khalilikhoo2@gmail.com>
2020-10-12 11:37:29 +0330

    test projects added

---
+++ Tests/ProductApi.Tests.Acceptance/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ProductApi.Tests.Acceptance
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
